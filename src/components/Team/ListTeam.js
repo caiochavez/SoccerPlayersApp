a0bb4f0718ccba67d7a0b18e26c5ba9deadcecfd
caiochavez <caaiochavez@gmail.com>
2018-11-07 16:55:33 -0300

    Busca de times

--- src/components/Team/ListTeam.js
+++ src/components/Team/ListTeam.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, ScrollView, AsyncStorage } from 'react-native'
-import { Header, Body, Title, Right, Icon, Container, List } from 'native-base'
+import { Header, Body, Title, Right, Icon, Container, List, Item, Input } from 'native-base'
 import { Actions } from 'react-native-router-flux'
 import ListTeamItem from './ListTeamItem'
 import dataTeams from './data'
 
 class ListTeam extends Component {
-  state = { teams: dataTeams }
+  state = { teams: dataTeams, searchText: '' }
 
   logout () {
     AsyncStorage.removeItem('token')
@@ -14,17 +14,48 @@ class ListTeam extends Component {
   }
 
   renderListTeamItem () {
-    const { teams } = this.state 
-    if (teams.length === 0) {
-      return (
-        <Text style={{ alignSelf: 'center', marginVertical: 200, fontSize: 20, color: '#006400' }}>
-          Nemhum Time Disponível
-        </Text>
-      )
+    const { teams, searchText } = this.state
+    if (searchText.length >= 3) {
+      const teamsFiltered = this.searchTeams(searchText)
+      if (teamsFiltered.length > 0) {
+        return teamsFiltered.map(team => {
+          return <ListTeamItem item={team} key={team.name} />
+        })
+      } else {
+        return (
+          <Text style={{ alignSelf: 'center', marginVertical: 200, fontSize: 20, color: '#006400' }}>
+            Nemhum Time Disponível
+          </Text>
+        )
+      }
     }
-    return teams.map(team => {
-      return <ListTeamItem item={team} />
+    else if (searchText.length === 0) {
+      if (teams.length === 0) {
+        return (
+          <Text style={{ alignSelf: 'center', marginVertical: 200, fontSize: 20, color: '#006400' }}>
+            Nemhum Time Disponível
+          </Text>
+        )
+      } else {
+        return teams.map(team => {
+          return <ListTeamItem item={team} key={team.name} />
+        })
+      }
+    } else {
+      return teams.map(team => {
+        return <ListTeamItem item={team} key={team.name} />
+      })
+    }
+  }
+
+  searchTeams (inputValue) {
+    const value = inputValue.toLowerCase()
+    const teamsFilter = this.state.teams.filter(team => {
+      const teamLowerCase = team.name.toLowerCase()
+      const resultSearch = teamLowerCase.search(value)
+      return resultSearch === 0
     })
+    return teamsFilter
   }
 
   render () {
@@ -40,6 +71,12 @@ class ListTeam extends Component {
             </TouchableOpacity>
           </Right>
         </Header>
+        <View searchbar rounded>
+          <Item>
+            <Icon name='ios-search' />
+            <Input placeholder='Buscar' onChangeText={value => this.setState({ searchText: value})} />
+          </Item>
+        </View>
         <ScrollView>
           <List>
             { this.renderListTeamItem() }
